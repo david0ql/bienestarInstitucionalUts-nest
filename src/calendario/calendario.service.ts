1c9185733a16de57ac1c06acb3405713e8223bb9
David Gomez <davidgggomez0421@gmail.com>
2022-12-19 22:05:22 -0500

    insercion de fechas

--- src/calendario/calendario.service.ts
+++ src/calendario/calendario.service.ts
@@ -13,8 +13,28 @@ export class CalendarioService {
   ) {}
 
   async create(createCalendarioDto: CreateCalendarioDto) {
-    const entrada = await this.calendarioRepository.save(createCalendarioDto);
-    return entrada;
+    const arr = [];
+    
+    const date1 = new Date(createCalendarioDto.startStr);
+    const date2 = new Date(createCalendarioDto.endStr);
+    const diffTime = Math.abs(+date2 - +date1);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; 
+
+    for (let index = 0; index < diffDays; index++) {
+      for (const key in createCalendarioDto.franjas) {
+        const calendario = await this.calendarioRepository.save({
+          ...createCalendarioDto,
+          fecha: date1.toString(),
+          idFranja: +createCalendarioDto.franjas[key],
+          correoPaciente: "asdasdadsd@gmail.com"
+        })
+        arr.push(calendario);
+        
+      }
+      date1.setDate(date1.getDate() + 1);
+    }
+    return arr;
+    
   }
 
   findAll(correoProfesional: string) {
